8a8b2b28fb9aa7edf29828ebcffcfa080b9897ca
Vũ Hải Hà <haiha210.gm@gmail.com>
2018-07-07 15:17:48 +0700

    user filter book by category

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,28 +1,20 @@
 $(document).on('turbolinks:load', function(){
-  navStick();
   goToIdPage();
   searchBarIcon();
 });
 
-navStick = function() {
-  $(window).scroll(function() {
-    if ($(document).scrollTop() > 50) {
-      $('nav').removeClass('navbar-custom');
-      $('nav').addClass('affix');
-    } else {
-      $('nav').removeClass('affix');
-      $('nav').addClass('navbar-custom');
-    }
-  });
-}
-
 goToIdPage = function(){
   $('.link-page').on('click', function(e){
     e.preventDefault();
-    var position = $($(this).attr('href')).offset().top;
-    $('body, html').animate({
-      scrollTop: position
-    }, 1000 );
+    if(window.location.pathname !== '/')
+      window.location.replace('/'+ $(this).attr('href'));
+    else {
+      var position = $($(this).attr('href')).offset().top;
+      $('body, html').animate({
+        scrollTop: position
+      }, 1000 );
+    }
+    
   });
 }
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,29 @@ class BooksController < ApplicationController
   before_action :load_book, only: :show
 
   def index
-    @search_books = Book.searchs(params[:search])
-                        .page(params[:page])
-                        .per Settings.page.per
+    load_book_index
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def show; end
 
   private
+
   def load_book
     @book = Book.find_by id: params[:id] || not_found
+  end
+
+  def load_book_index
+    books = Book.searchs(params[:search])
+                .ransack params[:q]
+    books.sorts = "name asc" if books.sorts.empty?
+    books = books.result(distinct: true)
+    @num_result = books.count
+    @filter_categories = Category.load_by_list_book(books.select(:id))
+    @search_books = books.page(params[:page])
+                         .per Settings.page.per
   end
 end
